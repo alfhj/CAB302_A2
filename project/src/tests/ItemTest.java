61a2dfeea122c2eb54fa37b9e74b982afb5d1da7
alfhj <alf.helge.jakobsen@gmail.com>
2018-04-27 13:39:07 +1000

    ItemTest

--- project/src/tests/ItemTest.java
+++ project/src/tests/ItemTest.java
@@ -16,7 +16,7 @@ public class ItemTest {
 	Item item;
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws StockException {
 		item = null;
 	}
 
@@ -26,15 +26,26 @@ public class ItemTest {
 		assertEquals("rice", item.getName());
 	}
 	
-	@Test (expected = Exception.class)
-	public void testEmptyName() throws Exception {
+	@Test (expected = StockException.class)
+	public void testEmptyName() throws StockException {
 		item = new Item("", 2, 3, 225, 300);
 	}
 	
 	@Test
-	public void testCost() {
+	public void testGetCost() {
 		item = new Item("rice", 2, 3, 225, 300);
 		assertEquals(2, item.getCost());
+	}
+	
+	@Test (expected = StockException.class)
+	public void testNegativeCost() throws StockException {
+		item = new Item("rice", -5, 3, 225, 300);
+	}
+
+	@Test
+	public void testZeroCost() throws StockException {
+		item = new Item("rice", 0, 3, 225, 300);
+		assertEquals(0, item.getCost());
 	}
 	
 	@Test
@@ -43,9 +54,14 @@ public class ItemTest {
 		assertEquals(3, item.getPrice());
 	}
 	
-	@Test (expected = Exception.class)
-	public void testNegativePrice() throws Exception {
-		item = new Item("rice", -5, 3, 225, 300);
+	@Test (expected = StockException.class)
+	public void testNegativePrice() throws StockException {
+		item = new Item("rice", 2, -3, 225, 300);
+	}
+	
+	@Test (expected = StockException.class)
+	public void testZeroPrice() throws StockException {
+		item = new Item("rice", 2, 0, 225, 300);
 	}
 	
 	@Test
@@ -53,15 +69,59 @@ public class ItemTest {
 		item = new Item("rice", 2, 3, 225, 300);
 		assertEquals(225, item.getReorderPoint());
 	}
+	
+	@Test (expected = StockException.class)
+	public void testNegativeReorderPoint() throws StockException {
+		item = new Item("rice", 2, 3, 225, 300);
+	}
+	
+	@Test
+	public void testZeroReorderPoint() {
+		item = new Item("rice", 2, 3, 0, 300);
+		assertEquals(0, item.getReorderPoint());
+	}
+
 	@Test
 	public void testGetReorderAmount() {
 		item = new Item("rice", 2, 3, 225, 300);
 		assertEquals(300, item.getReorderAmount());
 	}
-	@Test
-	public void testGetTemperature() {
-		item = new Item("rice", 2, 3, 225, 300);
-		assertEquals(null, item.getTemperature());
+	
+	@Test (expected = StockException.class)
+	public void testNegativeReorderAmount() throws StockException {
+		item = new Item("rice", 2, 3, 225, -200);
 	}
-
+	
+	@Test (expected = StockException.class)
+	public void testZeroReorderAmount() throws StockException {
+		item = new Item("rice", 2, 3, 225, 0);
+	}
+	
+	@Test
+	public void testGetTemperaturePositive() {
+		item = new Item("rice", 2, 3, 225, 300, 20);
+		assertEquals(-20, item.getTemperature());
+	}
+	
+	@Test
+	public void testGetTemperatureNegative() {
+		item = new Item("rice", 2, 3, 225, 300, -20);
+		assertEquals(-20, item.getTemperature());
+	}
+	
+	@Test
+	public void testNoTemperature() {
+		item = new Item("rice", 2, 3, 225, 300);
+		assertNull(item.getTemperature());
+	}
+	
+	@Test (expected = StockException.class)
+	public void testCrazyTemperature1() throws StockException {
+		item = new Item("rice", 2, 3, 225, 300, 150);
+	}
+	
+	@Test (expected = StockException.class)
+	public void testCrazyTemperature2() throws StockException {
+		item = new Item("rice", 2, 3, 225, 300, -150);
+	}
 }
