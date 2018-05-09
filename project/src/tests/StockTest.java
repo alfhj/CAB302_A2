ac4b8fe26250d0fb8fb453c99130387ee82ecb7b
Larysa McGookey <n9696563@qut.edu.au>
2018-05-09 17:41:55 +1000

    StockTest file was modified

--- project/src/tests/StockTest.java
+++ project/src/tests/StockTest.java
@@ -1,52 +1,50 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.Before;
 import org.junit.Test;
-
+import stock.Item;
 import stock.Stock;
+import stock.StockException;
 
 /**
  * 
  * @author lara09
  *
  */
-
 public class StockTest {
 	
 	Stock stock;
 	Map<Item, Integer> result;
 
 	@Before @Test
-	public void setUpStock() throws Exception {	
+	public void setUpStock(){	
 		stock = new Stock();
 		result = new HashMap<Item, Integer>();
 	}
 
 	@Test
-	public void testAddItems() {
+	public void testAddItems() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
-		stock.addItem(item1, 15);
+		stock.addItems(item1, 15);
 		result.put(item1, 15);
-		assertTrue(result.equals(stock.getStock()));
+		assertEquals(result, stock.getStock());
 	}
 	
 	@Test
-	public void testAddZero() {
+	public void testAddZero() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
-		stock.addItem(item1, 0);
+		stock.addItems(item1, 0);
 		result.put(item1, 0);
-		assertTrue(result.equals(stock.getStock()));
+		assertEquals(result, stock.getStock());
 	}
 	
 	@Test (expected = StockException.class)
 	public void testNegativeValue() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
-		stock.addItem(item1, -1);
+		stock.addItems(item1, -1);
 	}
 	
 	@Test (expected = StockException.class)
@@ -56,15 +54,15 @@ public class StockTest {
 	}
 	
 	@Test
-	public void testRemoveOneByOne() {
+	public void testRemoveOneByOne() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
 		stock.addItems(item1, 15);
-		for (int i = 0, i < 10, i++)
+		for (int i = 0; i < 10; i++)
 		{
 		stock.removeItems(item1, 1);
 		}
 		result.put(item1, 5);
-		assertTrue(result.equals(stock.getStock()));
+		assertEquals(result, stock.getStock());
 	}
 	
 	@Test (expected = StockException.class)
@@ -85,34 +83,34 @@ public class StockTest {
 	}
 		
 	@Test
-	public void testRemoveItem() {
+	public void testRemoveItem() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
 		stock.addItems(item1, 15);
 		stock.removeItems(item1, 2);
 		result.put(item1, 13);
-		assertTrue(result.equals(stock.getStock()));
+		assertEquals(result, stock.getStock());
 	}
 	
 	@Test
-	public void testRemoveAll() {
+	public void testRemoveAll() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
 		stock.addItems(item1, 15);
 		stock.removeItems(item1, 15);
 		result.put(item1, 0);
-		assertTrue(result.equals(stock.getStock()));
+		assertEquals(result, stock.getStock());
 	}
 	
 	@Test 
-	public void testAddSameItem() {
+	public void testAddSameItem() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
 		stock.addItems(item1, 15);
 		stock.addItems(item1, 20);
 		result.put(item1, 35);
-		assertTrue(result.equals(stock.getStock()));
+		assertEquals(result, stock.getStock());
 	}
 	
 	@Test 
-	public void testAddThree() {
+	public void testAddThree() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
 		Item item2 = new Item("flour", 2, 3, 225, 300);
 		Item item3 = new Item("pasta", 2, 3, 225, 300);
@@ -122,7 +120,7 @@ public class StockTest {
 		result.put(item1, 15);
 		result.put(item2, 2);
 		result.put(item3, 8);
-		assertTrue(result.equals(stock.getStock()));
+		assertEquals(result, stock.getStock());
 	}
 	
 	@Test (expected = StockException.class)
@@ -133,19 +131,12 @@ public class StockTest {
 	}
 	
 	@Test 
-	public void testRemoveZeroValue() {
+	public void testRemoveZeroValue() throws StockException {
 		Item item1 = new Item("rice", 2, 3, 225, 300);
 		stock.addItems(item1, 15);
 		stock.removeItems(item1, 0);
 		result.put(item1, 15);
-		assertTrue(result.equals(stock.getStock()));
-	}
-	
-	@Test 
-	public void testValue() {
-		Item item1 = new Item("rice", 2, 3, 225, 300);
-		stock.addItems(item1, 15);
-		stock.removeItems(item1, 0);
+		assertEquals(result, stock.getStock());
 	}
 	
 }
