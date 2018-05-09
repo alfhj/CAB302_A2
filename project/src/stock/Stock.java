8d3f9c84a4e4ade7aa4e13d026a965a94b77fe11
alfhj <n10142029@qut.edu.au>
2018-05-09 17:42:42 +1000

    Add Stock class

--- project/src/stock/Stock.java
+++ project/src/stock/Stock.java
@@ -1,10 +1,38 @@
 package stock;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  * @author alfhj
  *
  */
 public class Stock {
+
+	Map<Item, Integer> stock;
+	
+	public Stock() {
+		stock = new HashMap<Item, Integer>();
+	}
+	
+	public Map<Item, Integer> getStock() {
+		return stock;
+	}
+
+	public void addItems(Item item, int amount) throws StockException {
+		if (amount < 0) throw new StockException("Cannot add a negative amount of items.");
+		int oldAmount = 0;
+		if (stock.containsKey(item)) oldAmount = stock.get(item);
+		stock.put(item, oldAmount + amount);
+	}
+
+	public void removeItems(Item item, int amount) throws StockException {
+		if (! stock.containsKey(item)) throw new StockException("Cannot remove an item that is not in stock.");
+		if (amount < 0) throw new StockException("Cannot remove negative amounts.");
+		int newAmount = stock.get(item) - amount;
+		if (newAmount < 0) throw new StockException("Cannot remove more items than is available in stock.");
+		stock.put(item, newAmount);
+	}
 
 }
