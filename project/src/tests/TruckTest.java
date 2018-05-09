3ee7475e6b9fecd00cf146c275268c7c81fcdff7
alfhj <n10142029@qut.edu.au>
2018-05-09 18:20:55 +1000

    Sync

---
+++ project/src/tests/TruckTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author alfhj
+ *
+ */
+public class TruckTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
