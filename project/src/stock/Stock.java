16394b93c39f084a6c1fb5f9b3c9d4b6e117b40a
alfhj <alf.helge.jakobsen@gmail.com>
2018-05-10 00:32:19 +1000

    Typo

--- project/src/stock/Stock.java
+++ project/src/stock/Stock.java
@@ -11,29 +11,29 @@ import java.util.Map;
  */
 public class Stock {
 
-	Map<Item, Integer> stock;
+	Map<Item, Integer> items;
 	
 	public Stock() {
-		stock = new HashMap<Item, Integer>();
+		items = new HashMap<Item, Integer>();
 	}
 	
 	public Map<Item, Integer> getItems() {
-		return Collections.unmodifiableMap(stock);
+		return Collections.unmodifiableMap(items);
 	}
 
 	public void addItems(Item item, int amount) throws StockException {
 		if (amount < 0) throw new StockException("Cannot add a negative amount of items.");
 		int oldAmount = 0;
-		if (stock.containsKey(item)) oldAmount = stock.get(item);
-		stock.put(item, oldAmount + amount);
+		if (items.containsKey(item)) oldAmount = items.get(item);
+		items.put(item, oldAmount + amount);
 	}
 
 	public void removeItems(Item item, int amount) throws StockException {
-		if (! stock.containsKey(item)) throw new StockException("Cannot remove an item that is not in stock.");
+		if (! items.containsKey(item)) throw new StockException("Cannot remove an item that is not in stock.");
 		if (amount < 0) throw new StockException("Cannot remove negative amounts.");
-		int newAmount = stock.get(item) - amount;
+		int newAmount = items.get(item) - amount;
 		if (newAmount < 0) throw new StockException("Cannot remove more items than is available in stock.");
-		stock.put(item, newAmount);
+		items.put(item, newAmount);
 	}
 
 }
